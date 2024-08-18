521277722fd79306f00e9a765de8886209dbeb60
Steven Ballaret <135417330+SteveBllrt@users.noreply.github.com>
2024-08-18 23:51:20 +0800

    initial commit

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,95 +1,51 @@
-import { Link } from "expo-router";
-import React from "react";
-import { Text, View } from "react-native";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
+import { Text, View, TouchableOpacity, TextInput } from "react-native";
 
-export default function Page() {
+export default function App() {
   return (
-    <View className="flex flex-1">
-      <Header />
-      <Content />
-      <Footer />
-    </View>
-  );
-}
-
-function Content() {
-  return (
-    <View className="flex-1">
-      <View className="py-12 md:py-24 lg:py-32 xl:py-48">
-        <View className="px-4 md:px-6">
-          <View className="flex flex-col items-center gap-4 text-center">
-            <Text
-              role="heading"
-              className="text-3xl text-center native:text-5xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl"
-            >
-              Welcome to Project ACME
-            </Text>
-            <Text className="mx-auto max-w-[700px] text-lg text-center text-gray-500 md:text-xl dark:text-gray-400">
-              Discover and collaborate on amce. Explore our services now.
-            </Text>
-
-            <View className="gap-4">
-              <Link
-                suppressHighlighting
-                className="flex h-9 items-center justify-center overflow-hidden rounded-md bg-gray-900 px-4 py-2 text-sm font-medium text-gray-50 web:shadow ios:shadow transition-colors hover:bg-gray-900/90 active:bg-gray-400/90 web:focus-visible:outline-none web:focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
-                href="/"
-              >
-                Explore
-              </Link>
-            </View>
-          </View>
+    <View className='flex-1 justify-center items-center bg-white'>
+      <StatusBar style='auto' />
+      <Text className='text-center mt-3 text-2xl font-light text-orange-300'>
+        Login
+      </Text>
+      
+      <View className='mt-5 mx-5'>
+        <View>
+          <Text className='text-gray-400'>EMAIL:</Text>
+          <TextInput
+            placeholder='Enter Email...'
+            className='border border-dotted p-2 text-gray-500 border-amber-400 mt-1'
+          />
         </View>
-      </View>
-    </View>
-  );
-}
-
-function Header() {
-  const { top } = useSafeAreaInsets();
-  return (
-    <View style={{ paddingTop: top }}>
-      <View className="px-4 lg:px-6 h-14 flex items-center flex-row justify-between ">
-        <Link className="font-bold flex-1 items-center justify-center" href="/">
-          ACME
-        </Link>
-        <View className="flex flex-row gap-4 sm:gap-6">
-          <Link
-            className="text-md font-medium hover:underline web:underline-offset-4"
-            href="/"
-          >
-            About
-          </Link>
-          <Link
-            className="text-md font-medium hover:underline web:underline-offset-4"
-            href="/"
-          >
-            Product
-          </Link>
-          <Link
-            className="text-md font-medium hover:underline web:underline-offset-4"
-            href="/"
-          >
-            Pricing
-          </Link>
+        <View className='mt-3'>
+          <Text className='text-gray-400'>PASSWORD:</Text>
+          <TextInput
+            secureTextEntry
+            placeholder='Enter Password...'
+            className='border text-gray-500 border-dotted p-2 border-amber-400 mt-1'
+          />
         </View>
-      </View>
-    </View>
-  );
-}
 
-function Footer() {
-  const { bottom } = useSafeAreaInsets();
-  return (
-    <View
-      className="flex shrink-0 bg-gray-100 native:hidden"
-      style={{ paddingBottom: bottom }}
-    >
-      <View className="py-6 flex-1 items-start px-4 md:px-6 ">
-        <Text className={"text-center text-gray-700"}>
-          © {new Date().getFullYear()} Me
+        <TouchableOpacity className='bg-orange-300 p-3 mt-4'>
+          <Text className='text-center text-base text-white'>Login</Text>
+        </TouchableOpacity>
+
+        <Text className='text-center font-normal text-gray-500 text-base mt-3'>
+          OR
         </Text>
+        <View className='mt-4'>
+          <TouchableOpacity className='flex flex-row items-center justify-center p-2 bg-orange-300'>
+            <Text className='text-white mx-2 text-sm'>Sign In With Google</Text>
+          </TouchableOpacity>
+        </View>
+        <View className='mt-6 flex-row justify-center'>
+          <Text className=''>New to FreeCodeCamp? </Text>
+          <TouchableOpacity>
+            <Text className='text-amber-500'>Create an Account</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
   );
 }
+
