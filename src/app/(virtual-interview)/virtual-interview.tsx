b15fb4627f2d69972e5ea4f3e26471a303af4393
Steven Ballaret <135417330+SteveBllrt@users.noreply.github.com>
2024-09-18 07:19:48 +0800

    feat(virtual-interview): added recording to ui

--- src/app/(virtual-interview)/virtual-interview.tsx
+++ src/app/(virtual-interview)/virtual-interview.tsx
@@ -1,18 +1,79 @@
+import { sampleMessages } from "@/constants/sampleMessage";
 import { Message, Role } from "@/types/Chat";
 import { useUser } from "@clerk/clerk-expo";
 import React, { useEffect, useRef, useState } from "react";
 import { View, Text, FlatList, TouchableOpacity, Image } from "react-native";
-import { sampleMessages } from "../data/sampleMessage";
+import * as Speech from "expo-speech";
+import { Audio } from "expo-av";
 
 const VirtualInterview = () => {
-  const flatListRef = useRef<FlatList>(null);
   const { user } = useUser();
+  const flatListRef = useRef<FlatList>(null);
   const messages = sampleMessages;
-
+  const [isRecording, setIsRecording] = useState(false);
+  const [recording, setRecording] = useState<Audio.Recording | undefined>(
+    undefined
+  );
+  
   // Scroll to the bottom whenever messages update
   useEffect(() => {
     flatListRef.current?.scrollToEnd({ animated: true });
   }, [messages]);
+
+  const requestPermissions = async () => {
+    const { status } = await Audio.requestPermissionsAsync();
+    if (status !== "granted") {
+      console.error("Microphone permission not granted");
+    }
+  };
+
+  // Request permission
+  useEffect(() => {
+    requestPermissions();
+  }, []);
+
+  const startRecording = async () => {
+    if (isRecording) {
+      console.log("Already recording");
+      return;
+    }
+
+    try {
+      if (recording) {
+        await stopRecording();
+      }
+
+      const { recording: newRecording } = await Audio.Recording.createAsync(
+        Audio.RecordingOptionsPresets.HIGH_QUALITY
+      );
+      setRecording(newRecording);
+      setIsRecording(true);
+      console.log("Recording started");
+    } catch (err) {
+      console.error("Failed to start recording", err);
+    }
+  };
+
+  const stopRecording = async () => {
+    if (!recording) {
+      console.log("No recording to stop");
+      return;
+    }
+
+    console.log("Stopping recording...");
+    setIsRecording(false);
+    await recording.stopAndUnloadAsync();
+
+    const uri = recording.getURI();
+    console.log("Recording stopped and stored at", uri);
+    setRecording(undefined);
+  };
+
+  const speak = (message: string) => {
+    Speech.speak(message, {
+      rate: 1.0,
+    });
+  };
 
   const renderMessage = ({ item }: { item: Message }) => (
     <View
@@ -27,15 +88,16 @@ const VirtualInterview = () => {
               source={require("@/assets/images/savy.png")}
               className="w-5 h-5 rounded-full mr-1"
             />
-
             <Text className="text-sm"> Savy </Text>
           </View>
           <View className="bg-[#CDF1F8] p-4 rounded-lg max-w-[315px] border border-[#ADE3ED]">
             <Text className="text-base">{item.content}</Text>
+            <TouchableOpacity onPress={() => speak(item.content)}>
+              <Text className="text-blue-500">Speak</Text>
+            </TouchableOpacity>
           </View>
         </View>
       )}
-
       {item.role === Role.User && (
         <View className="flex items-end">
           <View className="flex-row items-center mb-1">
@@ -68,13 +130,22 @@ const VirtualInterview = () => {
         keyExtractor={(item, index) => index.toString()}
       />
       <View className="flex-row p-2 bg-white shadow-md justify-center border-gray-300 border">
-        <TouchableOpacity className="p-2">
-          <Image
-            source={require("@/assets/icons/mic.png")}
-            className="w-12 h-12 rounded-full mr-24"
-          />
-        </TouchableOpacity>
-        <TouchableOpacity className="p-2">
+        {isRecording ? (
+          <TouchableOpacity className="p-3" onPress={stopRecording}>
+            <Image
+              source={require("@/assets/icons/stop-mic.png")}
+              className="w-14 h-12 rounded-full mr-24"
+            />
+          </TouchableOpacity>
+        ) : (
+          <TouchableOpacity className="p-3" onPress={startRecording}>
+            <Image
+              source={require("@/assets/icons/mic.png")}
+              className="w-14 h-12 rounded-full mr-24"
+            />
+          </TouchableOpacity>
+        )}
+        <TouchableOpacity className="p-3">
           <Image
             source={require("@/assets/icons/video.png")}
             className="w-12 h-12 rounded-full"
