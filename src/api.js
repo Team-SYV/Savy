c0773ac623f4a864ee10c2d9a57f4380af7a242b
Steven Ballaret <135417330+SteveBllrt@users.noreply.github.com>
2024-09-17 23:42:56 +0800

    fix: url

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://stingray-beloved-terribly.ngrok-free.app/",
 });
 
 export const createJobInformation = async (jobData) => {
