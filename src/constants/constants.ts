5fb4617736ba6ff0b3d67c418a8c8e0b05bbbd46
Steven Ballaret <135417330+SteveBllrt@users.noreply.github.com>
2024-09-27 20:54:24 +0800

    refactor: improve job role choices

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -9,27 +9,43 @@ export const steps = [
 ];
 
 export const industry = [
-  { key: "IT", value: "Information Technology" },
+  { key: "SI", value: "Software Industry" },
   { key: "BPO", value: "Business Process Outsourcing" },
 ];
 
 export const jobRole = {
-  "Information Technology": [
+  "Software Industry": [
     { key: "software_developer", value: "Software Developer/Engineer" },
+    { key: "frontend_developer", value: "Frontend Developer" },
+    { key: "backend_developer", value: "Backend Developer" },
+    { key: "fullstack_developer", value: "Full Stack Developer" },
     { key: "qa_engineer", value: "QA Tester/Engineer" },
     { key: "business_analyst", value: "Business Analyst" },
     { key: "devops_engineer", value: "DevOps Engineer" },
-    { key: "project_manager", value: "Project Manager" },
+    { key: "ui_ux_designer", value: "UI/UX Designer" },
+    { key: "cloud_engineer", value: "Cloud Engineer" },
+    { key: "cybersecurity_analyst", value: "Cybersecurity Analyst" },
+    { key: "product_manager", value: "Product Manager" },
+    { key: "data_scientist", value: "Data Scientist" },
+    { key: "mobile_app_developer", value: "Mobile App Developer" },
     { key: "network_admin", value: "Network Administrator" },
     { key: "database_admin", value: "Database Administrator" },
-    { key: "cloud_engineer", value: "Cloud Engineer" },
+    { key: "software_architect", value: "Software Architect" },
   ],
   "Business Process Outsourcing": [
-    { key: "customer_service", value: "Customer Service Representatives" },
-    { key: "tech_support", value: "Technical Support Specialists" },
+    { key: "customer_service", value: "Customer Service Representative" },
+    { key: "tech_support", value: "Technical Support Specialist" },
     { key: "sales_telemarketing", value: "Sales and Telemarketing" },
+    { key: "virtual_assistant", value: "Virtual Assistant" },
+    { key: "data_entry", value: "Data Entry Specialist" },
+    { key: "claims_processor", value: "Claims Processor" },
     { key: "back_office", value: "Back-Office Operations" },
-    { key: "hr_outsourcing", value: "Human Resources (HR) Outsourcing" },
+    { key: "hr_outsourcing", value: "HR Outsourcing Specialist" },
+    { key: "training_specialist", value: "Training and Development Specialist" },
+    { key: "quality_analyst", value: "Quality Analyst" },
+    { key: "operations_manager", value: "Operations Manager" },
+    { key: "project_manager", value: "Project Manager" },
+    { key: "content_moderator", value: "Content Moderator" },
   ],
 };
 
