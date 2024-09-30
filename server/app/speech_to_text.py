c4fa5ec5342a9d555fc66b85ff377c17aa4b9d63
Steven Ballaret <135417330+SteveBllrt@users.noreply.github.com>
2024-09-30 15:16:41 +0800

    refactor: change audio translation to english only

--- server/app/speech_to_text.py
+++ server/app/speech_to_text.py
@@ -11,7 +11,8 @@ def transcribe_audio(audio_file_path):
     with open(audio_file_path, "rb") as audio_file:
         transcription = client.audio.transcriptions.create(
             model="whisper-1",
-            file=audio_file
+            file=audio_file,
+            language="en"
         )
         
     return transcription.text
