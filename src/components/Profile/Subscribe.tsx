a8d229ab9db589c72f2304a14b50200d531cb66c
Steven Ballaret <135417330+SteveBllrt@users.noreply.github.com>
2024-10-11 02:30:24 +0800

    refactor: fix margin

--- src/components/Profile/Subscribe.tsx
+++ src/components/Profile/Subscribe.tsx
@@ -6,7 +6,7 @@ import GetPro from "../Button/GetPro";
 const Subscribe = () => {
   return (
     <View>
-      <Pressable className="flex-row items-center justify-between p-4 border-b border-gray-500 rounded-lg mx-4 mt-20">
+      <Pressable className="flex-row items-center justify-between p-4 border-b border-gray-500 rounded-lg mx-4 mt-16">
         <View className="flex-row items-center">
           <MaterialIcons name="subscriptions" size={20} className="mr-2" />
           <Text className="text-base">Subscribe to pro plan</Text>
