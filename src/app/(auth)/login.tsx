64e05056831d0c80d0af1f18e309831d87b8e0d1
Steven Ballaret <135417330+SteveBllrt@users.noreply.github.com>
2024-10-10 22:36:16 +0800

    refactor: improve login ui

--- src/app/(auth)/login.tsx
+++ src/app/(auth)/login.tsx
@@ -1,19 +1,11 @@
 import React from "react";
 import { Link, Stack } from "expo-router";
 import LoginForm from "@/components/Form/LoginForm";
-import {
-  View,
-  Text,
-  Image,
-  ScrollView,
-  SafeAreaView,
-  Platform,
-} from "react-native";
+import { View, Text, ScrollView, SafeAreaView, Platform } from "react-native";
 import * as WebBrowser from "expo-web-browser";
 import GoogleSignInButton from "@/components/Button/GoogleSignInButton";
-import { LinearGradient } from "expo-linear-gradient";
 
-// hook to warm up and cool down the web browser for authentication.
+// Hook to warm up and cool down the web browser for authentication.
 export const useWarmUpBrowser = () => {
   React.useEffect(() => {
     if (Platform.OS !== "web") {
@@ -32,47 +24,31 @@ const Login = () => {
 
   return (
     <SafeAreaView className="bg-white min-h-full">
-      <LinearGradient colors={["#E3FAFF", "#FFFFFF"]}>
-        <ScrollView>
-          <Stack.Screen options={{ headerShown: false }} />
+      <ScrollView>
+        <Stack.Screen options={{ headerShown: false }} />
+        <Text className="text-[28px] font-bold mt-48 ml-5">Welcome Back!</Text>
+        <Text className="text-[15px] ml-6 mt-1">Sign in to your account</Text>
 
-          <View className="w-24 h-24 self-center mt-36">
-            <Image
-              source={require("@/assets/images/savy.png")}
-              className="w-full h-full"
-              resizeMode="contain"
-            />
-          </View>
+        <LoginForm />
 
-          <Text className="text-[32px] text-center font-medium"> Sign In </Text>
-          <Text className="text-center mt-1 text-[16px]">
-            Use your account to sign in below
-          </Text>
+        <View className="flex-row items-center justify-center mt-6 mb-8 mx-6">
+          <View className="flex-1 h-[1px] bg-gray-300" />
+          <Text className="text-center mx-2 text-gray-600"> or </Text>
+          <View className="flex-1 h-[1px] bg-gray-300" />
+        </View>
 
-          <LoginForm />
+        <GoogleSignInButton isLoading={isLoading} setIsLoading={setIsLoading} />
 
-          <View className="flex-row items-center justify-center">
-            <Text> Don't have an account? </Text>
-            <Link
-              href="/(auth)/register"
-              className="text-[#00657A] underline font-medium ml-1"
-            >
-              Create Account
-            </Link>
-          </View>
-
-          <View className="flex-row items-center justify-center mt-12 mb-12 mx-6">
-            <View className="flex-1 h-[1px] bg-gray-300" />
-            <Text className="text-center mx-2 text-gray-600"> or </Text>
-            <View className="flex-1 h-[1px] bg-gray-300" />
-          </View>
-
-          <GoogleSignInButton
-            isLoading={isLoading}
-            setIsLoading={setIsLoading}
-          />
-        </ScrollView>
-      </LinearGradient>
+        <View className="flex-row items-center justify-center mt-6">
+          <Text> Don't have an account? </Text>
+          <Link
+            href="/(auth)/register"
+            className="text-[#00657A] underline font-medium ml-1"
+          >
+            Create Account
+          </Link>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
