62ef5897b3e08a959e0291703a028e4126e3d1c5
Steven Ballaret <135417330+SteveBllrt@users.noreply.github.com>
2024-10-12 15:42:26 +0800

    refactor: fix margin

--- src/components/Profile/ShareFeedback.tsx
+++ src/components/Profile/ShareFeedback.tsx
@@ -13,7 +13,7 @@ const ShareFeedback = () => {
     <View>
       <Pressable
         onPress={navigateToFeedback}
-        className="flex-row items-center justify-between p-4 border-b border-gray-500 rounded-lg mx-4 mt-5"
+        className="flex-row items-center justify-between p-4 rounded-lg mx-4 mt-3"
       >
         <View className="flex-row items-center">
           <Entypo name="text-document" size={18} className="mr-2" />
