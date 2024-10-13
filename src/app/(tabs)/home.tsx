221e3429c1eac5b262f46e4e101c89299def9b6c
Steven Ballaret <135417330+SteveBllrt@users.noreply.github.com>
2024-10-13 18:07:25 +0800

    refactor: improve homepage ui

--- src/app/(tabs)/home.tsx
+++ src/app/(tabs)/home.tsx
@@ -2,16 +2,15 @@ import { View, Text, Image } from "react-native";
 import React from "react";
 import Card from "@/components/Card/Card";
 import HorizontalCard from "@/components/Card/HorizontalCard";
-import { Link } from "expo-router";
 
 const Home = () => {
   return (
-    <View className="p-4 bg-white min-h-full">
-      <View className="flex flex-row items-center mt-8">
-        <Text className="text-3xl font-semibold">Welcome</Text>
+    <View className="p-5 bg-white min-h-full">
+      <View className="flex flex-row items-center mt-4">
+        <Text className="text-[23px] font-semibold">Welcome</Text>
         <Image
           source={require("@/assets/icons/hand.png")}
-          className="ml-1 w-9 h-9"
+          className="ml-1 w-7 h-7"
         />
       </View>
 
@@ -19,26 +18,27 @@ const Home = () => {
         Get ready to ace interviews and advance your career with ease.
       </Text>
 
-      <Text className="text-base font-medium mt-12 mb-2">
+      <Text className="text-[14px] font-medium mt-7 mb-3">
         Practice Interview
       </Text>
-      <View className="flex flex-row items-center justify-center">
-        <Link href="/(virtual-interview)/job-information" className="mr-4">
-          <Card
-            imageSource={require("@/assets/images/virtual-interview.png")}
-            text="Talk with Virtual Interviewer"
-          />
-        </Link>
 
-        <Link href="/(record-yourself)/job-information">
-          <Card
-            imageSource={require("@/assets/images/record-yourself.png")}
-            text="Record Yourself"
-          />
-        </Link>
+      <View className="mb-4">
+        <Card
+          imageSource={require("@/assets/images/virtual-interview.png")}
+          text="Talk with Virtual Interviewer"
+          textClassName="text-[15px]"
+          buttonLink="/(virtual-interview)/job-information"
+        />
       </View>
 
-      <Text className="text-base font-medium mt-12 mb-3">
+      <Card
+        imageSource={require("@/assets/images/record-yourself.png")}
+        text="Record Yourself"
+        textClassName="text-[16px]"
+        buttonLink="/(record-yourself)/job-information"
+      />
+
+      <Text className="text-text-[14px] font-medium mt-7 mb-3">
         Read Interview Tips
       </Text>
 
